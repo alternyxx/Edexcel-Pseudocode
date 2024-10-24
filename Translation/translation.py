2f75304251e763f535787a15bef21fa6f3436d41
alternyxx <alternyxx@gmail.com>
2024-10-24 11:29:41 +0630

    comments :O

--- Translation/translation.py
+++ Translation/translation.py
@@ -24,56 +24,62 @@ class Translation():
         indent = ''
 
         # Common regex patterns
-        value = r"[^'\"\[\]]+(?:\[[^'\"]+\])?"
+        value = r"[^'\"\[\]]+(?:\[[^'\"]+\])*"
         # If anyone could clarify if a ' can be in a string in pseudocode, thatd be great
         string = r"'.*'"
         array = r"\[.*\]"
         # regex for variable / array index initialisation
-        var_index = r"[^ \[\]]+(?:\[[^ '\"]+\])?"
+        var_index = r"[^ \[\]]+(?:\[[^ '\"]+\])*"
         # Also whether or not its fine for the ' ?' to be a ' *' instead
         condition = rf"{value}(?: ?(?:=|!=|>|>=|<|<=) ?{value})?(?: (?:AND|OR|NOT) {value}(?: ?(?:=|!=|>|>=|<|<=) ?{value})?)*"
         arithmetic = r" ?(?:\+|-|\*|\^)"
+        comment = r'#.*'
+        end_of_line = rf' *(?:{comment})?(?:\n)?'
 
         for line in self.pseudocode:
 
             # declaration and initialization of a variable
-            if var_set := match(rf"{indent}SET ({var_index}) TO ({value}|{string}|{array})", line):
-                self._transpiled += f'{indent}{var_set.group(1)} = {var_set.group(2)}\n'
+            if var_set := fullmatch(rf"{indent}SET ({var_index}) TO ({value}|{string}|{array})({end_of_line})", line):
+                self._transpiled += f'{indent}{var_set.group(1)} = {var_set.group(2)}{str(var_set.group(3) or '')}'
 
-            elif line == '\n':
-                self._transpiled += line
+            elif empty_line := fullmatch(rf'({end_of_line})', line):
+                self._transpiled += f'{str(empty_line.group(1) or '')}'
 
             # print function
-            elif print_function := match(rf"{indent}SEND ({value}|{string}|{array}) TO DISPLAY", line):
-                self._transpiled += f'{indent}print({print_function.group(1).replace(' ', '')})\n'
+            elif print_function := fullmatch(rf"{indent}SEND ({value}|{string}|{array}) TO DISPLAY({end_of_line})", line):
+                self._transpiled += f'{indent}print({print_function.group(1).replace(' ', '')}){str(print_function.group(2) or '')}'
             
             # input function
-            elif input_function := match(rf"RECEIVE ({var_index}) FROM (?:\(STRING\)|\(INTEGER\)|\(CHARACTER\)) KEYBOARD", line):
-                self._transpiled = f'{indent}{input_function.group(1)} = input()'
+            elif input_function := fullmatch(rf"RECEIVE ({var_index}) FROM (?:\(STRING\)|\(INTEGER\)|\(CHARACTER\)) KEYBOARD({end_of_line})", line):
+                self._transpiled = f'{indent}{input_function.group(1)} = input(){str(input_function(2) or '')}'
 
             # If conditions
-            elif if_condition := match(rf'{indent}IF ({condition}) THEN', line):
-                self._transpiled += f'{indent}if {if_condition.group(1)}:\n'
+            elif if_condition := fullmatch(rf'{indent}IF ({condition}) THEN({end_of_line})', line):
+                self._transpiled += f'{indent}if {if_condition.group(1)}:{str(if_condition.group(2) or '')}'
                 indent += '    '
             
-            elif elif_condition := match(rf"{indent.removesuffix('    ')}ELSE IF ({condition}) THEN", line):
-                self._transpiled += f'{indent}'
+            elif elif_condition := fullmatch(rf"{indent.removesuffix('    ')}ELSE IF ({condition}) THEN({end_of_line})", line):
+                self._transpiled += f'{indent}elif {elif_condition.group(1)}:{str(elif_condition.group(2) or '')}'
             
-            elif fullmatch(rf'{indent.removesuffix('    ')}END IF', line):
+            elif fullmatch(rf'{indent.removesuffix('    ')}END IF({end_of_line})', line):
                 indent.removesuffix('    ')
 
-            elif while_loop := match(rf"WHILE (.+) DO", line):
-                self._transpiled += f'{indent}while {while_loop.group(1)}:\n'
+            elif while_loop := fullmatch(rf"WHILE ({condition}) DO({end_of_line})", line):
+                self._transpiled += f'{indent}while {while_loop.group(1)}:{str(while_loop.group(2) or '')}'
                 indent += '    '
             
-            elif match(rf'{indent.removesuffix('    ')}END WHILE', line):
+            elif fullmatch(rf'{indent.removesuffix('    ')}END WHILE({end_of_line})', line):
                 indent = indent.removesuffix('    ')
             
-            elif fullmatch(rf'REPEAT', line):
+            elif fullmatch(rf'REPEAT({end_of_line})', line):
                 ...
             
-            elif define := match(rf"FUNCTION", line):
+            elif define := match(rf"FUNCTION({end_of_line})", line):
                 ...
+            
+            else:
+                print(f'Syntax Error at line{self.pseudocode.index (line)}')
+                exit()
 
 
     #def transpile(self):
@@ -91,7 +97,6 @@ class Translation():
 
 
     def precompile(self):
-        temp = ''
-        temp.join(self.pseudocode)
-        #if search(r"RANDOM\(([0-9]+|[^\[\]]+(?:\[[^'\"]+\])?))\)", temp):
+        compilation = ''.join(self.pseudocode)
+        print(compilation)
             
