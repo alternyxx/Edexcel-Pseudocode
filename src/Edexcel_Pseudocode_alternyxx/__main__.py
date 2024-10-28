d629c76acbe7fc81b2329de862c43d83fbed0268
alternyxx <alternyxx@gmail.com>
2024-10-28 14:08:14 +0630

    packaging

---
+++ src/Edexcel_Pseudocode_alternyxx/__main__.py
@@ -0,0 +1,25 @@
+from Edexcel_Pseudocode_alternyxx import *
+
+def main() -> None:
+    # Class for handling CLI
+    args: cli.Args = cli.Args()
+    # File interpretation
+    try:
+        with open(args.args.Pseudocode_File) as file:
+            pseudocode: list = file.readlines()
+    # Doesnt Exist
+    except FileNotFoundError:
+        print("File doesn't exist")
+    else:
+        # Translate the file
+        translation = translation.Translation(pseudocode)
+        translation.compile()
+        translation.output_file('transpiled.py')
+        if args.args.Output_File:
+            translation.output_file(args.args.Output_File)
+
+        # Run the transpiled file
+        __import__('transpiled')
+
+if __name__ == "__main__":
+    main()    
